d7271a77cc2f57aa1fcf5f7f673ae8a36d1ba18c
Celestine Omin <celestine.omin@gmail.com>
2014-06-01 19:12:19 +0100

    Added a 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+include_once("header.php");
+?>
+
+	<div class="container">
+		Ooops. We think you missed your way :-). Come back <a href="/">home</a>
+	</div>
+
+<?php
+include_once("footer.php");
+?>
